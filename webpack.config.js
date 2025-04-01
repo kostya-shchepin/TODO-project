74af7089e0c6bb6d92c6cf8446b14e885f344012
Константин <kostya.shepin@yandex.ru>
2025-04-01 20:13:47 +0400

    01.04.25 new

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,45 @@
-const path = require("path"); // Импортируем модуль "path" для работы с путями файлов
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require('path') // Импортируем модуль "path" для работы с путями файлов
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
-    entry: "./src/index.js", // Точка входа для сборки проекта
+	entry: './src/index.js', // Точка входа для сборки проекта
 
-    output: {
-        filename: "bundle.js", // Имя выходного файла сборки
-        path: path.resolve(__dirname, "dist"), // Путь для выходного файла сборки
-    },
+	output: {
+		filename: 'bundle.js', // Имя выходного файла сборки
+		path: path.resolve(__dirname, 'dist'), // Путь для выходного файла сборки
+		clean: true,
+	},
 
-    module: {
-        rules: [
-            { test: /\.(less|css)$/, exclude: /src/, use: [MiniCssExtractPlugin.loader, "style-loader", "css-loader", "less-loader"] },
-            { test: /\.(js)$/, use: "babel-loader" },
-        ],
-    },
+	module: {
+		rules: [
+			{
+				test: /\.(css)$/,
+				use: [MiniCssExtractPlugin.loader, 'css-loader', 'less-loader'],
+			},
+			{
+				test: /\.(less)$/,
+				use: [MiniCssExtractPlugin.loader, 'css-loader', 'less-loader'],
+			},
+			{ test: /\.(js)$/, use: 'babel-loader' },
+		],
+	},
 
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: "./src/index.html",
-        }),
-        new MiniCssExtractPlugin({ filename: "[name].css" }),
-    ],
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: './src/index.html',
+		}),
+		new MiniCssExtractPlugin({
+			filename: '[name].css',
+		}),
+	],
 
-    devServer: {
-        static: {
-            directory: path.join(__dirname, "dist"), // Каталог для статики
-        },
-        open: true, // Автоматически открывать браузер
-    },
+	devServer: {
+		static: {
+			directory: path.join(__dirname, 'dist'), // Каталог для статики
+		},
+		open: true, // Автоматически открывать браузер
+	},
 
-    mode: "development", // Режим сборки
-};
+	mode: 'development', // Режим сборки
+}
