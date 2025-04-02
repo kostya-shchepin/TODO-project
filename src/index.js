a5126776e4fe826b3980c4d1a7c64fe978929c7b
Константин <kostya.shepin@yandex.ru>
2025-04-02 16:53:16 +0400

    02.04.25

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
-document.querySelectorAll(".delete-task").forEach((button) => {
-    button.addEventListener("click", (event) => {
-        event.target.parentElement.remove();
-    });
-});
+document.querySelectorAll('.delete-task').forEach(button => {
+	button.addEventListener('click', event => {
+		event.target.parentElement.remove()
+	})
+})
 
-document.querySelector(".add-task").addEventListener("click", function () {
-    document
-        .querySelector(".task-block")
-        .insertAdjacentHTML(
-            "afterend",
-            '<div class="task"><input type="checkbox" class="task-checkbox" /><p>Дело №2</p><button class="delete-task">Удалить</button></div>'
-        );
-});
+document.querySelector('.add-task').addEventListener('click', function () {
+	document
+		.querySelector('.task-block')
+		.insertAdjacentHTML(
+			'afterend',
+			'<div class="task"><input type="checkbox" class="task-checkbox" /><p>Дело №2</p><button class="delete-task">Удалить</button></div>'
+		)
+})
+
+require('./less/style.less')
