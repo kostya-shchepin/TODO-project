0d74017fd5f751c1db175576dc705f98678dea5a
Константин <kostya.shepin@yandex.ru>
2025-06-05 18:00:30 +0400

    05.06.25

--- src/index.js
+++ src/index.js
@@ -1,159 +1,125 @@
-// class Tasks {
-// 	constructor() {
-// 		this.tasks = JSON.parse(localStorage.getItem('tasks'))
-// 		this.tasks_check = JSON.parse(localStorage.getItem('tasks_check'))
-// 		this.popupBg = document.querySelector('.popup__bg') // Фон попап окна
-// 		this.popup = document.querySelector('.popup') // Само окно
-// 		this.openPopupButtons = document.querySelectorAll('.add-task') // Кнопки для показа окна
-// 		this.closePopupButton = document.querySelector('.close-popup')
-// 		this.initCheckboxHandlers()
-// 		this.deleteTask()
-// 	}
+class Tasks {
+	constructor() {
+		this.tasks = []
+		this.tasks_check = []
+		this.popupBg = document.querySelector('.popup__bg') // Фон попап окна
+		this.popup = document.querySelector('.popup') // Само окно
+		this.openPopupButtons = document.querySelectorAll('.add-task') // Кнопки для показа окна
+		this.closePopupButton = document.querySelector('.close-popup')
 
-// 	initCheckboxHandlers() {
-// 		document.querySelectorAll('.task-checkbox').forEach((checkbox, index) => {
-// 			if (localStorage.getItem('tasks_check')) {
-// 				this.tasks_check
-// 				if (this.tasks_check[index] === 1) {
-// 					checkbox.classList.add('checked')
-// 					checkbox.checked = true
-// 				}
-// 			} else {
-// 				this.tasks_check[index] = 0
-// 			}
+		this.openPopup()
+		this.closePopup()
+		this.addTask()
+		this.initTask()
+		this.initCheckboxHandlers()
+		this.deleteTask()
+	}
 
-// 			checkbox.addEventListener('click', event => {
-// 				if (event.target.checked) {
-// 					this.tasks_check[index] = 1
-// 					event.target.classList.add('checked')
-// 				} else {
-// 					this.tasks_check[index] = 0
-// 					event.target.classList.remove('checked')
-// 				}
-// 				localStorage.setItem('tasks_check', JSON.stringify(this.tasks_check))
-// 			})
-// 		})
-// 	}
+	openPopup() {
+		this.openPopupButtons.forEach(button => {
+			// Перебираем все кнопки
+			button.addEventListener('click', e => {
+				// Для каждой вешаем обработчик событий на клик
+				e.preventDefault() // Предотвращаем дефолтное поведение браузера
+				this.popupBg.classList.add('active') // Добавляем класс 'active' для фона
+				this.popup.classList.add('active') // И для самого окна
+			})
+		})
+	}
 
-// 	deleteTask() {
-// 		document.querySelectorAll('.delete-task').forEach((button, index) => {
-// 			button.addEventListener('click', event => {
-// 				event.target.parentElement.remove()
-// 				tasks.splice(index, 1)
-// 				localStorage.setItem('tasks', JSON.stringify(tasks))
-// 				tasks_check.splice(index, 1)
-// 				localStorage.setItem('tasks_check', JSON.stringify(tasks_check))
-// 			})
-// 		})
-// 	}
-// }
-
-let tasks = []
-let tasks_check = []
-if (localStorage.getItem('tasks')) {
-	tasks = JSON.parse(localStorage.getItem('tasks'))
-	tasks.forEach(task => {
-		document
-			.querySelector('.task-block')
-			.insertAdjacentHTML(
-				'beforeend',
-				`<div class="task"><input type="checkbox" class="task-checkbox" /><span>${task}</span><button class="delete-task"><div class="minus"></div></button></div>`
-			)
-	})
-}
-
-function initCheckboxHandlers() {
-	document.querySelectorAll('.task-checkbox').forEach((checkbox, index) => {
-		if (localStorage.getItem('tasks_check')) {
-			tasks_check = JSON.parse(localStorage.getItem('tasks_check'))
-			if (tasks_check[index] === 1) {
-				checkbox.classList.add('checked')
-				checkbox.checked = true
+	closePopup() {
+		document.addEventListener('click', e => {
+			// Вешаем обработчик на весь документ
+			if (e.target === this.popupBg) {
+				this.popupBg.classList.remove('active') // Убираем активный класс с фона
+				this.popup.classList.remove('active') // И с окна
 			}
-		} else {
-			tasks_check[index] = 0
+		})
+
+		this.closePopupButton.addEventListener('click', () => {
+			this.popupBg.classList.remove('active') // Убираем активный класс с фона
+			this.popup.classList.remove('active') // И с окна
+		})
+	}
+
+	initTask() {
+		if (localStorage.getItem('tasks')) {
+			this.tasks = JSON.parse(localStorage.getItem('tasks'))
+			this.tasks.forEach(task => {
+				document
+					.querySelector('.task-block')
+					.insertAdjacentHTML(
+						'beforeend',
+						`<div class="task"><input type="checkbox" class="task-checkbox" /><span>${task}</span><button class="delete-task"><div class="minus"></div></button></div>`
+					)
+			})
 		}
+	}
 
-		checkbox.addEventListener('click', event => {
-			if (event.target.checked) {
-				tasks_check[index] = 1
-				event.target.classList.add('checked')
-			} else {
-				tasks_check[index] = 0
-				event.target.classList.remove('checked')
+	addTask() {
+		document.querySelector('.popup button').addEventListener('click', () => {
+			const taskInput = document.querySelector('.task-input').value
+			if (taskInput == '') {
+				alert('Введите дело')
+				return
 			}
-			localStorage.setItem('tasks_check', JSON.stringify(tasks_check))
+			console.log(taskInput)
+			document
+				.querySelector('.task-block')
+				.insertAdjacentHTML(
+					'beforeend',
+					`<div class="task"><input type="checkbox" class="task-checkbox" /><span>${taskInput}</span><button class="delete-task"><div class="minus"></div></button></div>`
+				)
+			this.tasks.push(taskInput)
+			localStorage.setItem('tasks', JSON.stringify(this.tasks))
+			this.tasks_check.push(0)
+			localStorage.setItem('tasks_check', JSON.stringify(this.tasks_check))
+			this.popupBg.classList.remove('active') // Убираем активный класс с фона
+			this.popup.classList.remove('active') // И с окна
+			document.querySelector('.task-input').value = ''
+			this.initCheckboxHandlers()
+			this.deleteTask()
 		})
-	})
+	}
+
+	initCheckboxHandlers() {
+		document.querySelectorAll('.task-checkbox').forEach((checkbox, index) => {
+			if (localStorage.getItem('tasks_check')) {
+				this.tasks_check = JSON.parse(localStorage.getItem('tasks_check'))
+				if (this.tasks_check[index] === 1) {
+					checkbox.classList.add('checked')
+					checkbox.checked = true
+				}
+			} else {
+				this.tasks_check[index] = 0
+			}
+
+			checkbox.addEventListener('click', event => {
+				if (event.target.checked) {
+					this.tasks_check[index] = 1
+					event.target.classList.add('checked')
+				} else {
+					this.tasks_check[index] = 0
+					event.target.classList.remove('checked')
+				}
+				localStorage.setItem('tasks_check', JSON.stringify(this.tasks_check))
+			})
+		})
+	}
+
+	deleteTask() {
+		document.querySelectorAll('.delete-task').forEach((button, index) => {
+			button.addEventListener('click', event => {
+				event.target.parentElement.remove()
+				this.tasks.splice(index, 1)
+				localStorage.setItem('tasks', JSON.stringify(this.tasks))
+				this.tasks_check.splice(index, 1)
+				localStorage.setItem('tasks_check', JSON.stringify(this.tasks_check))
+			})
+		})
+	}
 }
 
-initCheckboxHandlers()
-
-function deleteTask() {
-	document.querySelectorAll('.delete-task').forEach((button, index) => {
-		button.addEventListener('click', event => {
-			event.target.parentElement.remove()
-			tasks.splice(index, 1)
-			localStorage.setItem('tasks', JSON.stringify(tasks))
-			tasks_check.splice(index, 1)
-			localStorage.setItem('tasks_check', JSON.stringify(tasks_check))
-		})
-	})
-}
-
-deleteTask()
-
-let popupBg = document.querySelector('.popup__bg') // Фон попап окна
-let popup = document.querySelector('.popup') // Само окно
-let openPopupButtons = document.querySelectorAll('.add-task') // Кнопки для показа окна
-let closePopupButton = document.querySelector('.close-popup') // Кнопка для скрытия окна
-
-openPopupButtons.forEach(button => {
-	// Перебираем все кнопки
-	button.addEventListener('click', e => {
-		// Для каждой вешаем обработчик событий на клик
-		e.preventDefault() // Предотвращаем дефолтное поведение браузера
-		popupBg.classList.add('active') // Добавляем класс 'active' для фона
-		popup.classList.add('active') // И для самого окна
-	})
-})
-
-document.addEventListener('click', e => {
-	// Вешаем обработчик на весь документ
-	if (e.target === popupBg) {
-		popupBg.classList.remove('active') // Убираем активный класс с фона
-		popup.classList.remove('active') // И с окна
-	}
-})
-
-closePopupButton.addEventListener('click', () => {
-	popupBg.classList.remove('active') // Убираем активный класс с фона
-	popup.classList.remove('active') // И с окна
-})
-
-document.querySelector('.popup button').addEventListener('click', () => {
-	const taskInput = document.querySelector('.task-input').value
-	if (taskInput == '') {
-		alert('Введите дело')
-		return
-	}
-	console.log(taskInput)
-	document
-		.querySelector('.task-block')
-		.insertAdjacentHTML(
-			'beforeend',
-			`<div class="task"><input type="checkbox" class="task-checkbox" /><span>${taskInput}</span><button class="delete-task"><div class="minus"></div></button></div>`
-		)
-	tasks.push(taskInput)
-	localStorage.setItem('tasks', JSON.stringify(tasks))
-	tasks_check.push(0)
-	localStorage.setItem('tasks_check', JSON.stringify(tasks_check))
-	console.log(tasks)
-	popupBg.classList.remove('active') // Убираем активный класс с фона
-	popup.classList.remove('active') // И с окна
-	document.querySelector('.task-input').value = ''
-	initCheckboxHandlers()
-	deleteTask()
-})
+new Tasks()
 
 require('./less/style.less')
